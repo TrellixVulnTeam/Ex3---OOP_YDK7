4f78e20500141734eb45f339b5c33d635374a7ee
97254 <adihoftman8@gmail.com>
2021-01-05 13:29:42 +0200

    update node + GraphAlgo

--- src/node.py
+++ src/node.py
@@ -1,8 +1,25 @@
 class NodeData:
 
-    def __init__(self, key: int, location = None):
+    def __init__(self, key: int, location: tuple = None):
         self.key = key
         self.location = location
         self.weight = 0.0
         self.info = ""
         self.tag = 0
+
+    def __str__(self):
+        return '(' + str(self.key) + ')'
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other):
+        return self.key == other.key and self.location == other.location
+
+    def __lt__(self, other):
+        t = other.tag - self.tag
+        return self.tag < other.tag
+
+    def __hash__(self):
+        return self.key
+
