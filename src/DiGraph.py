e0e3eb927678f551334323b61f97ed0b9590c428
97254 <adihoftman8@gmail.com>
2021-01-05 22:36:14 +0200

    add doc

--- src/DiGraph.py
+++ src/DiGraph.py
@@ -1,6 +1,7 @@
 from GraphInterface import GraphInterface
 import src.node as data
 
+
 class DiGraph(GraphInterface):
 
     def __init__(self):
@@ -15,22 +16,52 @@ class DiGraph(GraphInterface):
     def v_size(self) -> int:
         return self.sizeV
 
+    """
+        Returns the number of vertices in this graph
+        @return: The number of vertices in this graph
+        """
+
     def e_size(self) -> int:
+        """
+       Returns the number of edges in this graph
+       @return: The number of edges in this graph
+       """
         return self.sizeE
 
     def get_all_v(self) -> dict:
+        """return a dictionary of all the nodes in the Graph, each node is represented using a pair  (key, node_data)
+        """
         return self.nodes
 
     def all_in_edges_of_node(self, id1: int) -> dict:
+        """return a dictionary of all the nodes connected to (into) node_id ,
+        each node is represented using a pair (key, weight)
+         """
         return self.inE[id1]
 
     def all_out_edges_of_node(self, id1: int) -> dict:
+        """return a dictionary of all the nodes connected from node_id , each node is represented using a pair (key,
+        weight)
+        """
         return self.outE[id1]
 
     def get_mc(self) -> int:
+        """
+        Returns the current version of this graph,
+        on every change in the graph state - the MC should be increased
+        @return: The current version of this graph.
+        """
         return self.mC
 
     def add_edge(self, id1: int, id2: int, weight: float) -> bool:
+        """
+        Adds an edge to the graph.
+        @param id1: The start node of the edge
+        @param id2: The end node of the edge
+        @param weight: The weight of the edge
+        @return: True if the edge was added successfully, False o.w.
+        Note: If the edge already exists or one of the nodes dose not exists the functions will do nothing
+        """
         if id1 not in self.nodes.keys() or id2 not in self.nodes.keys():
             return False
         if id2 in self.edges[id1].keys():
@@ -51,6 +82,13 @@ class DiGraph(GraphInterface):
         return True
 
     def add_node(self, node_id: int, pos: tuple = None) -> bool:
+        """
+        Adds a node to the graph.
+        @param node_id: The node ID
+        @param pos: The position of the node
+        @return: True if the node was added successfully, False o.w.
+        Note: if the node id already exists the node will not be added
+        """
         if node_id not in self.nodes:
             self.nodes[node_id] = data.NodeData(node_id, pos)
             self.edges[node_id] = dict()
@@ -60,6 +98,12 @@ class DiGraph(GraphInterface):
         return False
 
     def remove_node(self, node_id: int) -> bool:
+        """
+        Removes a node from the graph.
+        @param node_id: The node ID
+        @return: True if the node was removed successfully, False o.w.
+        Note: if the node id does not exists the function will do nothing
+        """
         if node_id in self.nodes.keys():
             for n in self.inE.keys():
                 if node_id in self.inE[n].keys():
@@ -81,6 +125,13 @@ class DiGraph(GraphInterface):
         return False
 
     def remove_edge(self, node_id1: int, node_id2: int) -> bool:
+        """
+        Removes an edge from the graph.
+        @param node_id1: The start node of the edge
+        @param node_id2: The end node of the edge
+        @return: True if the edge was removed successfully, False o.w.
+        Note: If such an edge does not exists the function will do nothing
+        """
         if node_id2 in self.edges[node_id1].keys():
             del self.edges[node_id1][node_id2]
             del self.outE[node_id1][node_id2]
